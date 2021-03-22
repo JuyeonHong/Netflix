73e27fbe54ca5e2a554342f943cdaf965707c92b
hongjuyeon <coke1317@naver.com>
2021-03-22 13:51:30 +0900

    add project file

---
+++ Netflix/Netflix/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Netflix
+//
+//  Created by hongjuyeon_dev on 2021/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Netflix/Netflix/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Netflix
+//
+//  Created by hongjuyeon_dev on 2021/03/22.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
