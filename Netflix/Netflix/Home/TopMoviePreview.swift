8a6528c6e8366bd286b1fcc796613cc79110a8a8
hongjuyeon <coke1317@naver.com>
2021-03-22 17:51:48 +0900

    add TopMoviePreview file

---
+++ Netflix/Netflix/Home/TopMoviePreview.swift
@@ -0,0 +1,20 @@
+//
+//  TopMoviePreview.swift
+//  Netflix
+//
+//  Created by hongjuyeon_dev on 2021/03/22.
+//
+
+import SwiftUI
+
+struct TopMoviePreview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopMoviePreview_Previews: PreviewProvider {
+    static var previews: some View {
+        TopMoviePreview()
+    }
+}
