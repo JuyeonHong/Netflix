41b616856ecfea18d902ee04aa7b744b8c9956a2
hongjuyeon <coke1317@naver.com>
2021-03-22 16:17:23 +0900

    add HomeVM

--- Netflix/Netflix/Model/HomeVM.swift
+++ Netflix/Netflix/Model/HomeVM.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class HomeVM: ObservableObject {
+    
+    // String == Category
+    @Published var movies: [String: [Movie]] = [:]
+    
+    public var allCategories: [String] {
+        return movies.keys.map { String($0) }
+    }
+    
+    public func getMovie(forCat cat: String) -> [Movie] {
+        return movies[cat] ?? []
+    }
+    
+    init() {
+        setupMovies()
+    }
+    
+    func setupMovies() {
+        movies["Trending Now"] = exampleMovies
+        movies["Stand-Up Comedy"] = exampleMovies.shuffled()
+        movies["New Release"] = exampleMovies.shuffled()
+        movies["Watch It Again"] = exampleMovies.shuffled()
+        movies["Sci"] = exampleMovies.shuffled()
+    }
+}
