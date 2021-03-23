e329ddaadf30ee4c044360c2e9ccc6d1ca485d3d
hongjuyeon <coke1317@naver.com>
2021-03-23 14:00:05 +0900

    add SmallVerticalButton to reusing view

---
+++ Netflix/Netflix/Home/SmallVerticalButton.swift
@@ -0,0 +1,20 @@
+//
+//  SmallVerticalButton.swift
+//  Netflix
+//
+//  Created by hongjuyeon_dev on 2021/03/23.
+//
+
+import SwiftUI
+
+struct SmallVerticalButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SmallVerticalButton_Previews: PreviewProvider {
+    static var previews: some View {
+        SmallVerticalButton()
+    }
+}
