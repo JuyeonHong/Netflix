c2e1aa23a10f48306aca31065ce1158da5ffe405
hongjuyeon <coke1317@naver.com>
2021-03-23 16:21:19 +0900

    add white button file

---
+++ Netflix/Netflix/Home/WhiteButton.swift
@@ -0,0 +1,20 @@
+//
+//  WhiteButton.swift
+//  Netflix
+//
+//  Created by hongjuyeon_dev on 2021/03/23.
+//
+
+import SwiftUI
+
+struct WhiteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WhiteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        WhiteButton()
+    }
+}
