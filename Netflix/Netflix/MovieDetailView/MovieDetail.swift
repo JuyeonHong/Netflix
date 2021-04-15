38d5b50a64eebad1adf05077448f3e878db726a0
hongjuyeon <coke1317@naver.com>
2021-04-15 14:56:46 +0900

    add MovieDetailView

---
+++ Netflix/Netflix/MovieDetailView/MovieDetail.swift
@@ -0,0 +1,20 @@
+//
+//  MovieDetail.swift
+//  Netflix
+//
+//  Created by hongjuyeon_dev on 2021/04/15.
+//
+
+import SwiftUI
+
+struct MovieDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetail()
+    }
+}
