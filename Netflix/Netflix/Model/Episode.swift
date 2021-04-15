acd178e5dc4b93f421795cebc6b5e7d7366d5299
hongjuyeon <coke1317@naver.com>
2021-04-15 15:29:40 +0900

    add Episode struct

---
+++ Netflix/Netflix/Model/Episode.swift
@@ -0,0 +1,22 @@
+//
+//  Episode.swift
+//  Netflix
+//
+//  Created by hongjuyeon_dev on 2021/04/15.
+//
+
+import Foundation
+
+struct Episode: Identifiable {
+    var id = UUID().uuidString
+    
+    var name: String
+    var season: Int
+    var thumbnailImageURLString: String
+    var description: String
+    var length: Int
+    
+    var thumbnailURL: URL {
+        return URL(string: thumbnailImageURLString)!
+    }
+}
