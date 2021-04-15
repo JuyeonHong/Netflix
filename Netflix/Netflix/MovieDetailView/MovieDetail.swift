8620c4cb8ae8112743ca01d6d40c9e39500d2d77
hongjuyeon <coke1317@naver.com>
2021-04-15 14:57:22 +0900

    generate MovieInfoSubheadline View and MovieDetail View

--- Netflix/Netflix/MovieDetailView/MovieDetail.swift
+++ Netflix/Netflix/MovieDetailView/MovieDetail.swift
@@ -4,17 +4,70 @@
 //
 //  Created by hongjuyeon_dev on 2021/04/15.
 //
+// resume preview: option command p
 
 import SwiftUI
 
 struct MovieDetail: View {
+    var movie: Movie
+    
+    let screen = UIScreen.main.bounds
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        // close this view
+                    }, label: {
+                        Image(systemName: "xmark.circle")
+                            .font(.system(size: 28))
+                    })
+                }.padding(.horizontal, 22)
+                
+                ScrollView(.vertical, showsIndicators: false) {
+                    VStack {
+                        StandardHomeView(movie: movie)
+                            .frame(width: screen.width / 2.5)
+                        
+                        MovieInfoSubheadline(movie: movie)
+                        
+                    }
+                }
+                
+            }
+            .foregroundColor(.white)
+            
+        }
     }
 }
 
 struct MovieDetail_Previews: PreviewProvider {
     static var previews: some View {
-        MovieDetail()
+        MovieDetail(movie: exampleMovie1)
+    }
+}
+
+struct MovieInfoSubheadline: View {
+    var movie: Movie
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "hand.thumbsup.fill")
+                .foregroundColor(.white)
+            
+            Text("MOVIE YEAR")
+            
+            Text("RATING")
+            
+            Text("seasons")
+        }
+        .foregroundColor(.gray)
+        .padding(.vertical, 6)
     }
 }
