24fe53ebecc5dd57f774ced2e99e298982dfc3fa
hongjuyeon <coke1317@naver.com>
2021-04-15 17:03:22 +0900

    add Movie struct's properties and apply to Global Helpers

--- Netflix/Netflix/Helper/GlobalHelpers.swift
+++ Netflix/Netflix/Helper/GlobalHelpers.swift
@@ -4,18 +4,81 @@
 //
 //  Created by hongjuyeon_dev on 2021/03/22.
 //
+// 일괄 missing argument 추가하기: command option control F
 
 import Foundation
 import SwiftUI
 
-let exampleMovie1 = Movie(id: UUID().uuidString, name: "DARK", thumbnailURL: URL(string: "https://picsum.photos/200/300")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 1)
-let exampleMovie2 = Movie(id: UUID().uuidString, name: "Travelers", thumbnailURL: URL(string: "https://picsum.photos/200/300/")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 2, promotionHeadline: "Best Rated Show")
-let exampleMovie3 = Movie(id: UUID().uuidString, name: "Community", thumbnailURL: URL(string: "https://picsum.photos/200/301")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 3)
-let exampleMovie4 = Movie(id: UUID().uuidString, name: "Alone", thumbnailURL: URL(string: "https://picsum.photos/200/302")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 4, promotionHeadline: "New episode coming soon")
-let exampleMovie5 = Movie(id: UUID().uuidString, name: "Hannibal", thumbnailURL: URL(string: "https://picsum.photos/200/303")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 5)
-let exampleMovie6 = Movie(id: UUID().uuidString, name: "After Life", thumbnailURL: URL(string: "https://picsum.photos/200/304")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 6, promotionHeadline: "Watch Season 6 Now")
+let exampleMovie1 = Movie(id: UUID().uuidString,
+                          name: "DARK",
+                          thumbnailURL: URL(string: "https://picsum.photos/200/300")!,
+                          categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"],
+                          year: 2020,
+                          rating: "TV-MA",
+                          numberOfSeasons:1,
+                          defaultEpisodeInfo: exampleEpisodeInfo1,
+                          creators: "Christopher Nolan",
+                          cast: "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page")
+let exampleMovie2 = Movie(id: UUID().uuidString,
+                          name: "Travelers",
+                          thumbnailURL: URL(string: "https://picsum.photos/200/300/")!,
+                          categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"],
+                          year: 2020,
+                          rating: "TV-MA",
+                          numberOfSeasons: 2,
+                          defaultEpisodeInfo: exampleEpisodeInfo1,
+                          creators: "Christopher Nolan",
+                          cast: "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page",
+                          promotionHeadline: "Best Rated Show")
+let exampleMovie3 = Movie(id: UUID().uuidString,
+                          name: "Community",
+                          thumbnailURL: URL(string: "https://picsum.photos/200/301")!,
+                          categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"],
+                          year: 2020,
+                          rating: "TV-MA",
+                          numberOfSeasons: 3,
+                          defaultEpisodeInfo: exampleEpisodeInfo1,
+                          creators: "Christopher Nolan",
+                          cast: "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page")
+let exampleMovie4 = Movie(id: UUID().uuidString,
+                          name: "Alone",
+                          thumbnailURL: URL(string: "https://picsum.photos/200/302")!,
+                          categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"],
+                          year: 2020,
+                          rating: "TV-MA",
+                          numberOfSeasons: 4,
+                          defaultEpisodeInfo: exampleEpisodeInfo1,
+                          creators: "Christopher Nolan",
+                          cast: "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page",
+                          promotionHeadline: "New episode coming soon")
+let exampleMovie5 = Movie(id: UUID().uuidString,
+                          name: "Hannibal",
+                          thumbnailURL: URL(string: "https://picsum.photos/200/303")!,
+                          categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"],
+                          year: 2020,
+                          rating: "TV-MA",
+                          numberOfSeasons: 5,
+                          defaultEpisodeInfo: exampleEpisodeInfo1,
+                          creators: "Christopher Nolan",
+                          cast: "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page")
+let exampleMovie6 = Movie(id: UUID().uuidString,
+                          name: "After Life",
+                          thumbnailURL: URL(string: "https://picsum.photos/200/304")!,
+                          categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"],
+                          year: 2020,
+                          rating: "TV-MA",
+                          numberOfSeasons: 6,
+                          defaultEpisodeInfo: exampleEpisodeInfo1,
+                          creators: "Christopher Nolan",
+                          cast: "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page",
+                          promotionHeadline: "Watch Season 6 Now")
 
 let exampleMovies: [Movie] = [exampleMovie1, exampleMovie2, exampleMovie3, exampleMovie4, exampleMovie5, exampleMovie6]
+
+let exampleEpisodeInfo1 = CurrentEpisodeInfo(episodeName: "The Beginning and End of the Universe",
+                                             description: "This is a two-part British television series outlining the theory of the beginning of the universe and the theories about its ending",
+                                             season: 2,
+                                             episode: 1)
 
 extension LinearGradient  {
     static let blackOpacityGradient = LinearGradient(
