67c6d050031be2785cee6e498e0275d1bb6e1262
hongjuyeon <coke1317@naver.com>
2021-04-19 11:06:17 +0900

    drawing CustomTabSwitcher

--- Netflix/Netflix/MovieDetailView/CustomTabSwitcher.swift
+++ Netflix/Netflix/MovieDetailView/CustomTabSwitcher.swift
@@ -8,13 +8,61 @@
 import SwiftUI
 
 struct CustomTabSwitcher: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+    var tabs: [CustomTab]
+    
+    func widthForTab(_ tab: CustomTab) -> CGFloat {
+        return tab.rawValue.widthOfString(usingFont: .systemFont(ofSize: 16, weight: .bold))
     }
+    
+    
+    var body: some View {
+        VStack {
+            // custom tab picker
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(tabs, id: \.self) { tab in
+                        VStack {
+                            // red bar
+                            Rectangle()
+                                .frame(width: widthForTab(tab), height: 6)
+                            
+                            // button
+                            Button(action: {
+                                
+                            }, label: {
+                                Text(tab.rawValue)
+                                    .font(.system(size: 16, weight: .bold))
+                            })
+                            .buttonStyle(PlainButtonStyle())
+                        }
+                    }
+                }
+            }
+            
+            
+            
+            // Selected View
+            Text("SELECTED VIEW")
+        }
+        .foregroundColor(.white)
+    }
+}
+
+enum CustomTab: String {
+    case episodes = "EPISODES"
+    case trailers = "TRAILERS & MORE"
+    case more = "MORE LIKE THIS"
 }
 
 struct CustomTabSwitcher_Previews: PreviewProvider {
     static var previews: some View {
-        CustomTabSwitcher()
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            
+            CustomTabSwitcher(tabs: [.episodes, .trailers, .more])
+        }
     }
 }
