a1e11d14f5139063e770990e93fa8857eb085632
hongjuyeon <coke1317@naver.com>
2021-05-17 17:51:40 +0900

    create MoreLikeThis View

---
+++ Netflix/Netflix/MovieDetailView/MoreLikeThis.swift
@@ -0,0 +1,20 @@
+//
+//  MoreLikeThis.swift
+//  Netflix
+//
+//  Created by hongjuyeon_dev on 2021/05/17.
+//
+
+import SwiftUI
+
+struct MoreLikeThis: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MoreLikeThis_Previews: PreviewProvider {
+    static var previews: some View {
+        MoreLikeThis()
+    }
+}
