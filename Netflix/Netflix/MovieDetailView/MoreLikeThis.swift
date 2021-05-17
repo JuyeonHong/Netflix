11bef8db2a87c4775bd40172cf254069be38d0c2
hongjuyeon <coke1317@naver.com>
2021-05-17 17:52:34 +0900

    draw MoreLikeThis View

--- Netflix/Netflix/MovieDetailView/MoreLikeThis.swift
+++ Netflix/Netflix/MovieDetailView/MoreLikeThis.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct MoreLikeThis: View {
+    
+    var movies: [Movie]
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(0..<movies.count) { index in
+                    StandardHomeView(movie: movies[index])
+                }
+            }
+        }
     }
 }
 
 struct MoreLikeThis_Previews: PreviewProvider {
     static var previews: some View {
-        MoreLikeThis()
+        MoreLikeThis(movies: exampleMovies)
     }
 }
