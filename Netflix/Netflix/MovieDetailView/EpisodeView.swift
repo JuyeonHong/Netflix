0821918330bf5835466541fe482a5eccf053402e
hongjuyeon <coke1317@naver.com>
2021-05-20 14:39:29 +0900

    add EpisodeView

---
+++ Netflix/Netflix/MovieDetailView/EpisodeView.swift
@@ -0,0 +1,20 @@
+//
+//  EpisodeView.swift
+//  Netflix
+//
+//  Created by hongjuyeon_dev on 2021/05/20.
+//
+
+import SwiftUI
+
+struct EpisodeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EpisodeView_Previews: PreviewProvider {
+    static var previews: some View {
+        EpisodeView()
+    }
+}
