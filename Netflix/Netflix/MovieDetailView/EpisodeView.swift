ea7394c374574956204deca40862a35b28a07790
hongjuyeon <coke1317@naver.com>
2021-05-20 14:41:10 +0900

    draw EpisodeView and bind two variables to check what season is selected

--- Netflix/Netflix/MovieDetailView/EpisodeView.swift
+++ Netflix/Netflix/MovieDetailView/EpisodeView.swift
@@ -8,13 +8,51 @@
 import SwiftUI
 
 struct EpisodeView: View {
+    var episodes: [Episode]
+    
+    @Binding var showSeasonPicker: Bool
+    @Binding var selectedSeason: Int
+    
+    func getEpisode(forSeason season: Int) -> [Episode] {
+        return episodes.filter({ $0.season == season})
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // season picker
+            
+            HStack {
+                Button(action: {
+                    showSeasonPicker = true
+                }, label: {
+                    Group {
+                        Text("Season 1")
+                        Image(systemName: "chevron.down")
+                    }
+                    .font(.system(size: 16))
+                })
+                
+                Spacer()
+            }
+            
+            // episode list
+            ForEach(getEpisode(forSeason: selectedSeason)) { episode in
+                Text("test")
+            }
+            
+            Spacer()
+        }
+        .foregroundColor(.white)
     }
 }
 
 struct EpisodeView_Previews: PreviewProvider {
     static var previews: some View {
-        EpisodeView()
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            EpisodeView(episodes: allExampleEpisodes, showSeasonPicker: .constant(false), selectedSeason: .constant(1))
+        }
     }
 }
