dd875470214682e7e7e639073fa0911dc8f05fff
hongjuyeon <coke1317@naver.com>
2021-07-06 14:41:55 +0900

    add SearchBar on new group Search add colors to apply on SearchBar

---
+++ Netflix/Netflix/Helper/Colors.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  Netflix
+//
+//  Created by hongjuyeon_dev on 2021/07/06.
+//
+
+import Foundation
---
+++ Netflix/Netflix/Search/SearchBar.swift
@@ -0,0 +1,20 @@
+//
+//  SearchBar.swift
+//  Netflix
+//
+//  Created by hongjuyeon_dev on 2021/07/06.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBar()
+    }
+}
