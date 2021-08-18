a39551444c6c9e5418fd23286c3a5c33f0cdfea3
hongjuyeon <coke1317@naver.com>
2021-08-18 14:36:40 +0900

    add SearchResultsGrid

---
+++ Netflix/Netflix/Search/SearchResultsGrid.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResultsGrid.swift
+//  Netflix
+//
+//  Created by hongjuyeon_dev on 2021/08/18.
+//
+
+import SwiftUI
+
+struct SearchResultsGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchResultsGrid_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchResultsGrid()
+    }
+}
