240c9879a68ffd56f1a0bf2f287c5b26a47a9795
hongjuyeon <coke1317@naver.com>
2021-09-10 13:54:27 +0900

    draw commingsoom ui

--- Netflix/Netflix/CommingSoon/CommingSoon.swift
+++ Netflix/Netflix/CommingSoon/CommingSoon.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct CommingSoon: View {
+    
+    @State private var showNotificationList = false
+    
+    @State private var navBarHidden = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+//        NavigationView {
+            Group {
+                ZStack {
+                    Color.black.edgesIgnoringSafeArea(.all)
+                    
+                    ScrollView {
+                        VStack {
+                            NotificationBar(showNotificationList: $showNotificationList)
+                            
+                            Text("For each loop of cells")
+                        }
+                    }
+                    .foregroundColor(.white)
+                }
+                
+                NavigationLink(
+                    destination: Text("Notification List"),
+                    isActive: $showNotificationList,
+                    label: {
+                        EmptyView()
+                    })
+                    .navigationTitle("")
+                    .navigationBarHidden(navBarHidden)
+                    .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification), perform: { _ in
+                        self.navBarHidden = true
+                    })
+                    .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification), perform: { _ in
+                        self.navBarHidden = false
+                    })
+                
+            }
+//        }
     }
 }
 
