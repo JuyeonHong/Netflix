aa83f18a136eb18ccab457d9d458036dfcce0c95
hongjuyeon <coke1317@naver.com>
2021-09-16 15:23:32 +0900

    add MoviePreviewCell and draw UI

---
+++ Netflix/Netflix/Preview/MoviePreviewCell.swift
@@ -0,0 +1,47 @@
+//
+//  MoviePreviewCell.swift
+//  Netflix
+//
+//  Created by hongjuyeon_dev on 2021/09/16.
+//
+
+import SwiftUI
+import KingfisherSwiftUI
+
+struct MoviePreviewCell: View {
+    
+    var movie: Movie
+    
+    let colors: [Color] = [.yellow, .gray, .pink, .red, .blue, .green, .orange]
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            KFImage(movie.thumbnailURL)
+                .resizable()
+                .scaledToFill()
+                .clipShape(Circle())
+                .overlay(
+                    Circle()
+                        .stroke(lineWidth: 3.0)
+                        .foregroundColor(colors.randomElement())
+            )
+            
+            Image(movie.previewImageName)
+                .resizable()
+                .scaledToFit()
+                .offset(y: -20)
+                .frame(height: 65)
+        }
+    }
+}
+
+struct MoviePreviewCell_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            MoviePreviewCell(movie: exampleMovie1)
+                .frame(width: 165, height: 50)
+        }
+    }
+}
