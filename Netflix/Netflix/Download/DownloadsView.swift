ae9b6bee502688c72a766c8d44a212db998b4f82
hongjuyeon <coke1317@naver.com>
2021-09-16 14:28:13 +0900

    add DownloadViews

---
+++ Netflix/Netflix/Download/DownloadsView.swift
@@ -0,0 +1,20 @@
+//
+//  DownloadsView.swift
+//  Netflix
+//
+//  Created by hongjuyeon_dev on 2021/09/16.
+//
+
+import SwiftUI
+
+struct DownloadsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DownloadsView_Previews: PreviewProvider {
+    static var previews: some View {
+        DownloadsView()
+    }
+}
