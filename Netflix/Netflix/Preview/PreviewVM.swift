a00ed2221241d3008ef6cfab045be4a59a0bf701
hongjuyeon <coke1317@naver.com>
2021-11-22 13:39:44 +0900

    add PreViewVM, PreviewView

---
+++ Netflix/Netflix/Preview/PreviewVM.swift
@@ -0,0 +1,8 @@
+//
+//  PreviewVM.swift
+//  Netflix
+//
+//  Created by hongjuyeon_dev on 2021/11/12.
+//
+
+import Foundation
---
+++ Netflix/Netflix/Preview/PreviewView.swift
@@ -0,0 +1,20 @@
+//
+//  PreviewView.swift
+//  Netflix
+//
+//  Created by hongjuyeon_dev on 2021/11/12.
+//
+
+import SwiftUI
+
+struct PreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PreviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        PreviewView()
+    }
+}
